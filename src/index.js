33951449f393e912098129deae46264507cfabc5
joshmeidl <josh_meidl@Joshs-MacBook-Pro.local>
2020-03-01 14:18:44 -0800

    quiz page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Quiz from "./components/Quiz";
+import Landing from "./components/landing_page/landing";
 
-ReactDOM.render(<Quiz />, document.getElementById("root"));
+ReactDOM.render(<Landing />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
