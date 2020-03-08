083b867e4171840a057ce64c7bdd28ba9ec12d6e
joshmeidl <josh_meidl@Joshs-MacBook-Pro.local>
2020-03-08 16:47:24 -0700

    build

---
+++ build/precache-manifest.f7f3bbde742f4b98af82339c9b526380.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a425a00114ad3d34c12828a638d2f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e888d22cf9cc4233d15a",
+    "url": "/static/css/main.dcde65db.chunk.css"
+  },
+  {
+    "revision": "6facd864b6be3e6e636d",
+    "url": "/static/js/2.3073ff6a.chunk.js"
+  },
+  {
+    "revision": "65d8a6274ca582a852e80e7d0c66f569",
+    "url": "/static/js/2.3073ff6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e888d22cf9cc4233d15a",
+    "url": "/static/js/main.f2397a97.chunk.js"
+  },
+  {
+    "revision": "c66653f7beccbbe77079",
+    "url": "/static/js/runtime-main.a7ee45c1.js"
+  },
+  {
+    "revision": "7c7adde0213325e31d6542e1758b1401",
+    "url": "/static/media/home1.7c7adde0.jpg"
+  },
+  {
+    "revision": "bfb4a450dda73c1cceb05d0bcd57061d",
+    "url": "/static/media/logo5.bfb4a450.png"
+  }
+]);
